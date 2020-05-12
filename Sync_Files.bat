cbbe9b518109dfc0b893bc8f43b4787610a2b905
Matthew Hahn <matthewhahn73@gmail.com>
2020-05-12 12:48:00 -0500

    Update Sync_Files.bat

--- Sync_Files.bat
+++ Sync_Files.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
-cd "F:\Work Files\Python Scripts\Sync Work Files"
-set p1=-hostdir="F:\Work Files"
-set p2=-destdir="C:\Users\matth\Desktop\Files\Back-Ups\Work Materials"
+cd "<location of script>"
+set p1=-hostdir="<path to external drive directory>"
+set p2=-destdir="<path to destination directory>"
 python -u Sync_Work_Files.py -syncall %p1% %p2%
 pause
